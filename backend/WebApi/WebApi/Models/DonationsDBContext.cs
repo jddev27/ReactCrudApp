0af0a454234f64cee6c28d80089e047aeb8e7a07
jddev27 <Juan.deLunaBeltre@extendhealth.com>
2020-12-28 14:25:58 -0700

    initial commit

---
+++ backend/WebApi/WebApi/Models/DonationsDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models
+{
+    public class DonationsDBContext: DbContext
+    {
+        public DonationsDBContext(DbContextOptions<DonationsDBContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<DCandidate> Candidates { get; set; }
+    }
+}
